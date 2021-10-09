a3d8fba6b72bf0b4b9db7536d30759882c1f5b78
kaitlynphan <92197314+kaitlynphan@users.noreply.github.com>
2021-10-08 23:44:58 -0500

    test change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          pp poo poo
         </p>
         <a
           className="App-link"
